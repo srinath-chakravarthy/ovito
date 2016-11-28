422cf4bfc607ed5ea4e35987adc72f5dcddddbee
s.chakravarthy <s.chakravarthy@neu.edu>
2016-11-28 08:59:54 -0500

    export_vtk cell field for regionid of triangles

--- src/core/Core.h
+++ src/core/Core.h
@@ -83,6 +83,7 @@
 #include <QBrush>
 #include <QCommandLineParser>
 #include <QGuiApplication>
+#include <QFile>
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 2, 0)
 #  error "OVITO requires at least Qt 5.2"
