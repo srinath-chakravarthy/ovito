56b1042e3f658767a0ec8b0d9f6842c29c6f8356
s.chakravarthy <s.chakravarthy@neu.edu>
2017-04-26 17:58:31 -0400

    extra merges from master

--- src/opengl_renderer/OpenGLTextPrimitive.cpp
+++ src/opengl_renderer/OpenGLTextPrimitive.cpp
@@ -22,6 +22,7 @@
 #include <core/Core.h>
 #include "OpenGLTextPrimitive.h"
 #include "OpenGLSceneRenderer.h"
+#include 
 
 namespace Ovito { OVITO_BEGIN_INLINE_NAMESPACE(Rendering) OVITO_BEGIN_INLINE_NAMESPACE(Internal)
 
--- src/plugins/crystalanalysis/resources/python/ovito/modifiers/crystalanalysis/__init__.py
+++ src/plugins/crystalanalysis/resources/python/ovito/modifiers/crystalanalysis/__init__.py
@@ -3,18 +3,21 @@ import ovito.modifiers
 import ovito.modifiers.particles
 
 # Load the native code modules.
-import Particles
-import ParticlesImporter
-import ParticlesExporter
-import CrystalAnalysis
+import ovito.plugins.Particles
+import ovito.plugins.ParticlesImporter
+import ovito.plugins.ParticlesExporter
+import ovito.plugins.CrystalAnalysis
 
 # Inject modifier classes into parent module.
-ovito.modifiers.ConstructSurfaceModifier = CrystalAnalysis.ConstructSurfaceModifier
-ovito.modifiers.SmoothDislocationsModifier = CrystalAnalysis.SmoothDislocationsModifier
-ovito.modifiers.SmoothSurfaceModifier = CrystalAnalysis.SmoothSurfaceModifier
-ovito.modifiers.DislocationAnalysisModifier = CrystalAnalysis.DislocationAnalysisModifier
-ovito.modifiers.ElasticStrainModifier = CrystalAnalysis.ElasticStrainModifier
-ovito.modifiers.GrainSegmentationModifier = CrystalAnalysis.GrainSegmentationModifier
+ovito.modifiers.ConstructSurfaceModifier = ovito.plugins.CrystalAnalysis.ConstructSurfaceModifier
+ovito.modifiers.SmoothDislocationsModifier = ovito.plugins.CrystalAnalysis.SmoothDislocationsModifier
+ovito.modifiers.SmoothSurfaceModifier = ovito.plugins.CrystalAnalysis.SmoothSurfaceModifier
+ovito.modifiers.DislocationAnalysisModifier = ovito.plugins.CrystalAnalysis.DislocationAnalysisModifier
+ovito.modifiers.ElasticStrainModifier = ovito.plugins.CrystalAnalysis.ElasticStrainModifier
+ovito.modifiers.GrainSegmentationModifier = ovito.plugins.CrystalAnalysis.GrainSegmentationModifier
+ovito.modifiers.__all__ += ['ConstructSurfaceModifier', 'SmoothDislocationsModifier', 'SmoothSurfaceModifier', 'DislocationAnalysisModifier',
+            'ElasticStrainModifier']
+ovito.modifiers.__all__ += ['GrainSegmentationModifier']
 
 # Copy enum list.
 ovito.modifiers.ElasticStrainModifier.Lattice = ovito.modifiers.DislocationAnalysisModifier.Lattice
